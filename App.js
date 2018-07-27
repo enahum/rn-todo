974f08807abaf3aa333ac1a7d37c5535bf9ce43b
Elias Nahum <nahumhbl@gmail.com>
2018-07-27 18:35:59 -0400

    Add PN support (#11)

--- App.js
+++ App.js
@@ -1,24 +1,60 @@
 import React, {PureComponent} from 'react';
 import {Provider} from 'react-redux';
-import {StatusBar} from 'react-native';
+import {Platform, StatusBar} from 'react-native';
 import {PersistGate} from 'redux-persist/integration/react';
+import {Notifications, Permissions} from 'expo';
 
 import AppNavigation from 'app/screens';
 import LoadingScreen from 'app/screens/loading';
 import {store, persistor} from 'app/store';
 
 export default class App extends PureComponent {
-  render() {
-    return (
-        <Provider store={store}>
-            <PersistGate
-                loading={<LoadingScreen/>}
-                persistor={persistor}
-            >
-                <StatusBar barStyle="light-content"/>
-                <AppNavigation/>
-            </PersistGate>
-        </Provider>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        if (Platform.OS === 'android') {
+            Notifications.createChannelAndroidAsync('test-notifications', {
+                name: 'Tests',
+                sound: true,
+                priority: 'high'
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.requestNotificationsPermission();
+        this.listener = Notifications.addListener((event) => {
+            if (event.origin === 'selected') {
+                // TODO: Do something when a notification is tapped.
+            } else if (event.origin === 'received' && Platform.OS === 'ios') {
+                //TODO: when the app is in the foreground for iOS no notification
+                // is presented by the OS and we need to handle that in-app.
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.listener.remove();
+    }
+
+    requestNotificationsPermission = async () => {
+        const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+        if (status !== 'granted') {
+            Permissions.askAsync(Permissions.NOTIFICATIONS);
+        }
+    };
+
+    render() {
+        return (
+            <Provider store={store}>
+                <PersistGate
+                    loading={<LoadingScreen/>}
+                    persistor={persistor}
+                >
+                    <StatusBar barStyle="light-content"/>
+                    <AppNavigation/>
+                </PersistGate>
+            </Provider>
+        );
+    }
 }
--- app/actions/todos.js
+++ app/actions/todos.js
@@ -1,8 +1,29 @@
+import {Notifications} from 'expo';
 import {TodoTypes} from 'app/action_types';
 
+const NOTIFY_OFFSET = 15 * 1000;
+
 export function createTodo(data, notify) {
-    return {
-        type: TodoTypes.ADD_TODO,
-        ...data
+    return async (dispatch) => {
+        let notificationId;
+        if (notify) {
+            notificationId = await Notifications.scheduleLocalNotificationAsync({
+                title: 'Recordatorio de Tarea',
+                body: data.text,
+                data: {
+                    message: data.text,
+                },
+                android: {
+                    channelId: 'test-notifications'
+                }
+            }, {
+                time: data.date - NOTIFY_OFFSET
+            });
+        }
+        dispatch({
+            type: TodoTypes.ADD_TODO,
+            ...data,
+            notificationId,
+        });
     };
 }
